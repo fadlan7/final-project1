eaaf56501b543aa20445dec0514477106ea5fc08
fadlan7 <inifadlan7@gmail.com>
2022-10-14 21:12:41 +0700

    database name

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'cobaFinalProject1',
+  database: 'finalProject1',
   password: 'admin',
   port: '5432',
 });
